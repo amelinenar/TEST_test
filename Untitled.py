8f428aa0d561469402410dff8d833917e3d9393a
Nguenang Ameline Raissa <97096086+amelinenar@users.noreply.github.com>
2024-03-10 15:01:57 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,28 +1,31 @@
 
-def addition(numb1,numb2):
-    return numb1 + numb2
-def subtraction( numb1,numb2):
-    return numb1 -  numb2
-def diviion(numb1, numb2):
-    return numb1/numb2
-def multiplication(numb1,numb2):
-    return numb1*numb2
+class Calculator:
+    def __init__(self):
+        pass
+    def add(self,numb1,numb2):
+        return numb1 + numb2
+    def subtraction(self,numb1,numb2):
+        return numb1 -  numb2
+    def diviion(self,numb1, numb2):
+        return numb1/numb2
+    def multiplication(self,numb1,numb2):
+        return numb1*numb2
     
-numb1 = int(input("Enter the first number"))
-numb2 = int(input("Enter the second number"))
+#numb1 = int(input("Enter the first number"))
+#numb2 = int(input("Enter the second number"))
 #print(addition(numb1,numb2))
-operator = print("Enter the operation you want to perform")
-print("1: ADDITION")
-print("2: SUBTRACTION")
-print("3: MULTIPLICATION")
-print("4: DIVISION")
-operator= int(input(" "))
-if operator == 1:
-    print("result =",addition(numb1,numb2))
-if operator == 2:
-    print("result =", subtraction(numb1,numb2))
-if operator == 3:
-    print("result =", multiplication(numb1, numb2))
-if operator == 4:
-    print("result =", diviion(numb1,numb2))
+#operator = print("Enter the operation you want to perform")
+#print("1: ADDITION")
+#print("2: SUBTRACTION")
+#print("3: MULTIPLICATION")
+#print("4: DIVISION")
+#operator= int(input(" "))
+#if operator == 1:
+   # print("result =",addition(numb1,numb2))
+#if operator == 2:
+    #print("result =", subtraction(numb1,numb2))
+#if operator == 3:
+    #print("result =", multiplication(numb1, numb2))
+#if operator == 4:
+   # print("result =", diviion(numb1,numb2))
 
